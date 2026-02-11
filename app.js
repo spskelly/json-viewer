047915004044bbef69b68725bb58980647628c11
spskelly <103201627+spskelly@users.noreply.github.com>
2026-02-11 02:27:49 -0500

    update scope

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // register service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('/service-worker.js', { scope: '/json-viewer/' })
     .then(reg => console.log('Service worker registered', reg))
     .catch(err => console.log('Service worker registration failed:', err));
 }
